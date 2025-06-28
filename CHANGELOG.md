4db79bcae09268a73c2e61cf8014c1d73b92dd1e
PhilLavoie <PhilLavoie@users.noreply.github.com>
2025-06-28 06:08:44 -0400

    Optional logger name for prefixing stuffs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,22 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.2.0] - 2025-06-28
+
+### Added
+
+- Allowed the optional use of a name for the logger (used as prefix in messages).
+- Added the `from` factory function to build from explicit Console object.
+
+### Removed
+
+- The previous `create` factory function would accept an optional console object. In general,
+we will favor `from` type factory functions when they inject optional dependecies into
+the object. This is because the normal use of this class is without passing the `Console`
+object. The ability to pass a `Console` object is meant more for tests, so in order to
+streamline the most likely use case, we've taken out the `logger` optional parameter from
+`create` and moved it to `from`.
+
 ## [0.1.0] - 2025-06-15
 
 ### Added
--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -1,14 +1,45 @@
-import { expect } from "@infra-blocks/test";
+import { expect, fake, sinon } from "@infra-blocks/test";
 import { ConsoleLogger } from "../../src/index.js";
 
 describe("index", function () {
-  describe("ConsoleLogger", function () {
+  describe(ConsoleLogger.name, function () {
     describe("create", function () {
       it("should work with no parameters", function () {
         const logger = ConsoleLogger.create();
         expect(logger).to.be.instanceOf(ConsoleLogger);
       });
     });
+    for (const level of ["trace", "debug", "info", "warn", "error"] as const) {
+      describe(level, function () {
+        it("should dispatch message to console", function () {
+          const log = sinon.fake();
+          const console = fake<Console>({
+            [level]: log,
+          });
+          const logger = ConsoleLogger.from({ console });
+          logger[level]("hello?");
+          expect(log).to.have.been.calledWith("hello?");
+        });
+        it("should should dispatch message and parameters to console", function () {
+          const log = sinon.fake();
+          const console = fake<Console>({
+            [level]: log,
+          });
+          const logger = ConsoleLogger.from({ console });
+          logger[level]("hello %s", "world");
+          expect(log).to.have.been.calledWith("hello %s", "world");
+        });
+        it("should should dispatch name, message and parameters to console", function () {
+          const log = sinon.fake();
+          const console = fake<Console>({
+            [level]: log,
+          });
+          const logger = ConsoleLogger.from({ console, name: "test-logger" });
+          logger[level]("hello %s", "world");
+          expect(log).to.have.been.calledWith("test-logger: hello %s", "world");
+        });
+      });
+    }
     describe("isTraceEnabled", function () {
       it("should always return true", function () {
         const logger = ConsoleLogger.create();
